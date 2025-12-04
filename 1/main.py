eb59627e748154ca134f79aed601e09e20f08045
Arthur Guimarães <aarthgmendessm@gmail.com>
2025-12-04 20:23:48 -0300

    refactoring counter code

--- 1/main.py
+++ 1/main.py
@@ -1,5 +1,13 @@
 from contador import contar_palavras
 
-frase = input('Digite uma frase: ')
-quantidade = contar_palavras(frase)
-print(f'A frase tem {quantidade} de palavras.')
+frase = input('Digite uma frase: ').strip()
+if not frase:
+    print("ERRO: Nenhuma frase foi digitada.")
+else:
+    resultado = contar_palavras(frase)
+    if resultado:
+        print("Contagem de Palavras: ")
+        for palavra, quantidade in resultado.items():
+            print(f"{palavra}: {quantidade}")
+    else:
+        print(f"Nenhuma palavra válida foi encontrada.")        
